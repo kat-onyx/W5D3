6c90d3388e3167b5d45a92baf20d1992be1760a5
kat-onyx <sarah.kathryn.peters@gmail.com>
2018-11-21 16:17:13 -0800

    Complete task up to p03

--- skeleton/lib/controller_base.rb
+++ skeleton/lib/controller_base.rb
@@ -8,25 +8,47 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    if !@already_built_response
+      @res.status = 302
+      @res.header["Location"] = url
+      @already_built_response = true
+    else
+      raise "Don't render twice"
+    end
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
-  def render_content(content, content_type)
+  def render_content(content, content_type = 'text/html')
+    if !@already_built_response
+      @res['Content-Type'] = content_type
+      @res.write(content)
+      @already_built_response = true
+    else
+      raise "Don't render twice"
+    end
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    path = File.dirname(__FILE__)
+    path = File.join(path, '../views', "#{self.class.to_s.underscore}",  "#{template_name}.html.erb")
+    file_content = File.read(path)
+    erb_code = ERB.new(file_content).result(binding)
+    render_content(erb_code)
   end
 
   # method exposing a `Session` object
